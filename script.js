c30d2ec9d82a8924ee76b25ae1538995be0cde07
natnael abebaw <natnaell70a@gmail.com>
2023-10-30 12:41:29 -0400

    complete hero and about section for the final design

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
+"use strict";
 const sections = document.querySelectorAll("section");
 
 const slideSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
   if (!entry.isIntersecting) return;
   entry.target.classList.remove("hidden");
 };
@@ -16,3 +16,47 @@ sections.forEach((section) => {
   observer.observe(section);
   section.classList.add("hidden");
 });
+
+function moveSquare(squareDivClass, pathDivClass) {
+  const rectPath = document.querySelector(`.${pathDivClass}`);
+  const arrow = document.querySelector(`.${squareDivClass}`);
+  let height = Math.floor(rectPath.getBoundingClientRect().height);
+  let width = Math.floor(rectPath.getBoundingClientRect().width);
+  let x = 0;
+  let y = 0;
+
+  setInterval(function () {
+    if (x < width && y <= 0) {
+      x++;
+      arrow.style.left = `${x}px`;
+    }
+    if (x >= width && y < height) {
+      y = y + 1;
+      arrow.style.top = `${y}px`;
+    }
+    if (y >= height && x > 0) {
+      x--;
+      arrow.style.left = `${x}px`;
+    }
+    if (x <= 0 && y > 0) {
+      y--;
+      arrow.style.top = `${y}px`;
+    }
+  }, 10);
+}
+
+const autoText = document.querySelector(".auto-text");
+const text = "Passionate";
+let x = 1;
+setInterval(() => {
+  autoText.textContent = text.slice(0, x);
+
+  if (x <= text.length) {
+    x++;
+  } else {
+    x = 1;
+  }
+}, 300);
+
+moveSquare("arrow-top", "square-top");
+moveSquare("arrow-bottom", "square-bottom");
