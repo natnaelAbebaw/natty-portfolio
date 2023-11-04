bd66823c94d7c4ce96d8d5d0b71c7f9cd1c05217
natnael abebaw <natnaell70a@gmail.com>
2023-11-04 19:25:22 -0400

    add responsiveness

--- script.js
+++ script.js
@@ -1,67 +1,56 @@
 "use strict";
 const sections = document.querySelectorAll("section");
 
-const slideSection = function (entries, observer) {
+const slideup = function (entries, observer) {
   const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove("hidden");
+  if (entry.isIntersecting) {
+    entry.target.classList.remove("hidden");
+  }
 };
 
-const observer = new IntersectionObserver(slideSection, {
+const observer = new IntersectionObserver(slideup, {
   root: null,
-  threshold: 0.4,
+  threshold: 0.1,
 });
 
-sections.forEach((section) => {
-  observer.observe(section);
-  section.classList.add("hidden");
+sections.forEach((el) => {
+  observer.observe(el);
+  el.classList.add("hidden");
 });
 
-function moveSquare(squareDivClass, pathDivClass) {
-  const rectPath = document.querySelector(`.${pathDivClass}`);
-  const arrow = document.querySelector(`.${squareDivClass}`);
-  // const height = Math.floor(rectPath.getBoundingClientRect().height);
-  // const width = Math.floor(rectPath.getBoundingClientRect().width);
-  let x = 0;
-  let y = 0;
+const humburger = document.querySelector(".humburger");
+const close = document.querySelector(".close");
 
-  setInterval(function () {
-    const height = Math.floor(rectPath.getBoundingClientRect().height);
-    const width = Math.floor(rectPath.getBoundingClientRect().width);
+const header = document.querySelector(".header");
 
-    if (x < width && y <= 0) {
-      x++;
-      arrow.style.left = `${x}px`;
-    }
-    if (x >= width && y < height) {
-      y = y + 1;
-      arrow.style.top = `${y}px`;
-    }
-    if (y >= height && x > 0) {
-      x--;
-      arrow.style.left = `${x}px`;
-    }
-    if (x <= 0 && y > 0) {
-      y--;
-      arrow.style.top = `${y}px`;
-    }
-  }, 10);
+humburger.addEventListener("click", function () {
+  header.classList.add("active-menu");
+});
+
+close.addEventListener("click", function () {
+  header.classList.remove("active-menu");
+});
+
+document.addEventListener("click", function (e) {
+  if (
+    !e.target.classList.contains("humburger") &&
+    !e.target.classList.contains("nav-links")
+  ) {
+    header.classList.remove("active-menu");
+  }
+});
+
+const links = document.querySelectorAll("header ul a");
+
+function changeLinkState() {
+  const index = sections.length;
+
+  if (--index && window.scrollY + 50 < sections[index].scrollTop) {
+  }
+  links.forEach((link) => link.classList.remove("active"));
+  links[index].classList.remove("active");
 }
 
-const autoText = document.querySelector(".auto-text");
-const text = "Passionate";
-let x = 1;
-setInterval(() => {
-  autoText.textContent = text.slice(0, x);
+changeLinkState();
 
-  if (x <= text.length) {
-    x++;
-  } else {
-    x = 1;
-  }
-}, 300);
-
-moveSquare("arrow-top", "square-top");
-moveSquare("arrow-bottom", "square-bottom");
-
-// observer2.observe(bg);
+window.addEventListener("scroll", changeLinkState);
