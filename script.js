4ba11a016e6caea6f9172c0aef2a657bdc78e881
natnael abebaw <natnaell70a@gmail.com>
2023-12-16 07:05:04 -0500

    add real works

--- script.js
+++ script.js
@@ -58,3 +58,84 @@ function changeLinkState() {
 changeLinkState();
 
 window.addEventListener("scroll", changeLinkState);
+
+const worksList = [
+  {
+    img: "https://drive.google.com/uc?id=1y8qWmmxlOj91Nv5kNeRFH7T-48dRant-",
+    title: "Web-snake",
+    type: "web development",
+    catagory: "front-end",
+    github: "natnaelAbebaw",
+    liveLink: "https://web-snake.netlify.app/",
+    text: `Dive into nostalgia with my web-based Snake Game! Crafted solely with HTML, CSS, and JavaScript—no frameworks involved. Enjoy sleek, responsive design, intuitive controls, and dynamic gameplay as you slither through a visually pleasing environment. Challenge your friends, track high scores, and experience timeless fun in this minimalist, yet engaging, creation. Explore the beauty of simplicity in web development with this classic Snake Game.`,
+  },
+  {
+    img: "https://drive.google.com/uc?id=1e4-vNX4990yqn8ixd2VowvqjWUr9Pzmt",
+    title: "Note book",
+    type: "web development",
+    catagory: "front-end",
+    github: " https://github.com/natnaelAbebaw/Note-taking-app",
+    liveLink: "https://note-taking-app-silk-one.vercel.app/",
+    text: `Elevate your note-taking game with my React Notebook App. Seamlessly group notes into notebooks for easy organization. Effortlessly search through your notes, finding what you need in an instant. Mark favorites for quick access to your most important thoughts. This app, built with React, combines functionality and simplicity for an enhanced note-taking experience.`,
+  },
+  ,
+];
+
+let workTypeState = "";
+const worksBox = document.querySelector(".works-box");
+
+const workBtns = document.querySelectorAll(".work-btn");
+
+workBtns.forEach((btn) =>
+  btn.addEventListener("click", function (e) {
+    const id = e.target.dataset.id;
+    workTypeState = id;
+    workBtns.forEach((el) => el.classList.remove("btn-underline-active"));
+    e.target.classList.add("btn-underline-active");
+    render();
+  })
+);
+
+function renderworks(works) {
+  let html = "";
+
+  works.forEach((work) => {
+    if (workTypeState && work.catagory !== workTypeState) return "";
+    html += `
+      <li class="work">
+        <div class="work-img">
+          <img
+            src=${work.img}
+            alt="project image"
+          />
+        </div>
+    
+        <div class="work-content">
+          <div>
+            <h3>${work.title}</h3>
+            <span>${work.type}</span>
+          </div>
+          <p>
+          ${work.text}
+          </p>
+        </div>
+        <div class="work-btns-box">
+             <a href="${work.github}" class="btn btn--fill"> Source code </a>
+             <a href="${work.liveLink}" class="btn btn--fill"> See Live </a>
+        </div>
+      </li>
+    `;
+  });
+
+  return html;
+}
+
+const Works = document.createElement("div");
+Works.classList.add("works");
+
+function render() {
+  Works.innerHTML = renderworks(worksList);
+  worksBox.appendChild(Works);
+}
+
+render();
